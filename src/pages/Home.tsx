44eb8c5cc5a49b00a8e215d099ab437c3789bd9d
IsmailovSardorbek <ismailovsardorbek792@gmail.com>
2023-10-25 17:05:37 +0500

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import Hero from '../components/Hero/Hero'
 import Products from '../components/Products/Products'
 import Speaker from '../components/Speaker/Speaker'
-import Earphone from '../components/Earphones/Earphone'
+import Earphone from '../components/Earphone/Earphone'
 
 export default function Home() {
   return (
